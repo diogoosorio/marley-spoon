fe6178c3c57be6c8ceae87942430dda520c27b07
Diogo Osório <diogo.g.osorio@gmail.com>
2019-11-01 14:35:02 +0000

    Introduces react-router-dom to the application

---
+++ recipes-ui/src/constants/routes.js
@@ -0,0 +1,7 @@
+const Routes = {
+  ROOT: '/',
+  RECIPE_LIST: '/recipes',
+  RECIPE_DETAIL: '/recipes/:id'
+}
+
+export default Routes
--- recipes-ui/src/containers/AppContainer/AppContainer.jsx
+++ recipes-ui/src/containers/AppContainer/AppContainer.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
-import { Button } from 'antd';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Redirect,
+  Route
+} from 'react-router-dom';
+import Routes from '../../constants/routes';
 import './AppContainer.css';
 
 const AppContainer = () => (
-  <>
-    <Button>Demo Antd</Button>
-  </>
+  <div className="app">
+    <Router>
+      <Switch>
+        <Route path={Routes.ROOT} exact>
+          <Redirect to={Routes.RECIPE_LIST} />
+        </Route>
+
+        <Route path={Routes.RECIPE_LIST} exact>
+          <h1>Recipe List</h1>
+        </Route>
+
+        <Route path={Routes.RECIPE_DETAIL} exact>
+          <h1>Recipe Detail</h1>
+        </Route>
+      </Switch>
+    </Router>
+  </div>
 )
 
 export default AppContainer;
